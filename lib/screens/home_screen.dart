5a6bab1e7e7c8dd8deb736759d266395c88e7afe
Aiden <74317894+GInterns@users.noreply.github.com>
2023-10-06 00:24:51 +0800

    initial push

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        title: Text("My Cooking Buddy"),
+        centerTitle: true,
+      ),
+    );
+  }
+}
---
+++ lib/style/app_style.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class AppStyle {
+  static Color mainColor = Color(0xFF000633);
+  static Color accentColor = Color(0xFF000633);
+}
