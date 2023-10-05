ad9c95f93a3ea46a9f33d016d14b786e8686a17d
Aiden <74317894+GInterns@users.noreply.github.com>
2023-10-06 04:59:37 +0800

    chore: push before merge from feat/navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fyp_app/screens/home_screen.dart';
@@ -5,7 +6,19 @@ import 'package:fyp_app/screens/home_screen.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await signInAnonymously();
   runApp(const MyApp());
+}
+
+Future<void> signInAnonymously() async {
+  try {
+    await FirebaseAuth.instance.signInAnonymously();
+    // User is signed in anonymously
+    print("---Signed in anoymously---");
+  } catch (e) {
+    // Handle errors
+    print("Error signing in: ${e.toString()}");
+  }
 }
 
 class MyApp extends StatelessWidget {
